a222b62c1b3a126ba5a06337f9e58514e5a214e8
Martin Staab <martin.e.staab@gmail.com>
2025-04-04 18:24:48 -0700

    Moved: spilt brainfloat implementation into seperate file

---
+++ libbrainfloat.js
@@ -0,0 +1,300 @@
+const DEFAULT_CELL_SIZE = 256;
+const DEFAULT_IDLE_INTERVAL = 50;
+
+class BFParseError extends Error {}
+
+class BFloatParseError extends Error {}
+
+async function idle() {
+    await new Promise(resolve => setTimeout(resolve, 0));
+}
+
+function parseBF(bf) {
+    let result = [];
+    let stack = [result];
+    for (let char of bf) {
+        let frame = stack.at(-1);
+
+        let basicOpMap = {
+            "+": "incr",
+            "-": "decr",
+            "<": "left",
+            ">": "right",
+            ".": "output",
+            ",": "input",
+            "~": "dump"
+        };
+
+        if (char in basicOpMap) {
+            frame.push({action: basicOpMap[char]});
+        } else if (char == "[") {
+            let block = [];
+            frame.push({action: "loop", block});
+            stack.push(block);
+        } else if (char == "]") {
+            stack.pop();
+            if (stack.length == 0) {
+                throw new BFParseError("Unmatched close bracket");
+            }
+        }
+    }
+    if (stack.length > 1) {
+        throw new BFParseError("Unmatched open bracket");
+    }
+    return result;
+}
+
+function getWrapNumFn(env) {
+    let cellSize = env.cellSize || DEFAULT_CELL_SIZE;
+    if (cellSize == -1) return n => n;
+    if (cellSize == 256) return n => n & 0xFF;
+    return n => {
+        n %= cellSize;
+        return n < 0 ? n + cellSize : n;
+    };
+}
+
+async function executeParsedBF(env, parsed) {
+    if (env.onStart) env.onStart(parsed);
+
+    let lastIdle = Date.now();
+
+    let mem = [0];
+    let i = 0;
+
+    let stack = [parsed];
+    let idxs = [0]; // the indices in the stack frames
+
+    let wrapNum = getWrapNumFn(env);
+
+    while (stack.length > 0) {
+        let frame = stack.at(-1);
+        let idx = idxs.at(-1);
+
+        if (frame.length == idx) { // we're at the end of the loop
+            let now = Date.now();
+            let idleInterval = (env.idleInterval
+                || DEFAULT_IDLE_INTERVAL);
+            if (now - lastIdle > idleInterval) {
+                if (env.isStopRequested()) return;
+                await idle();
+                lastIdle = now;
+            }
+
+            if (mem[i] == 0 || stack.length == 1) {
+                stack.pop();
+                idxs.pop();
+            } else {
+                idxs[idxs.length-1] = 0;
+            }
+
+            continue;
+        }
+
+        let cmd = frame[idx];
+        let action = cmd.action;
+
+        idxs[idxs.length-1] = idx+1;
+
+        switch (action) {
+        case "incr":
+            mem[i] = wrapNum(mem[i]+1);
+            break;
+        case "decr":
+            mem[i] = wrapNum(mem[i]-1);
+            break;
+        case "left":
+            if (i > 0) i--;
+            break;
+        case "right":
+            i++;
+            if (mem.length <= i) {
+                mem.push(0);
+            }
+            break;
+        case "output":
+            if (env.sendOutput) {
+                env.sendOutput(String.fromCharCode(mem[i]));
+            }
+            break;
+        case "input":
+            let input;
+            if (env.getInput) {
+                input = await env.getInput();
+            } else {
+                input = 0;
+            }
+            if (input == -1) return;
+            mem[i] = input;
+            lastIdle = Date.now();
+            break;
+        case "dump":
+            if (env.ouputDump) {
+                env.ouputDump(mem, i);
+            }
+            break;
+        case "loop":
+            if (mem[i] != 0) {
+                stack.push(cmd.block);
+                idxs.push(0);
+            }
+            break;
+        }
+    }
+}
+
+function optimizeTextBF(bf) {
+    let emptyEquivalents = [
+        "<>", "><", "-+", "+-", 
+        /(?<=\])\[[^\[\]]+\]/g, "[]"
+    ];
+    let changes = true;
+    while (changes) {
+        changes = false;
+        for (let phrase of emptyEquivalents) {
+            let newBF = bf.replaceAll(phrase, "");
+            if (newBF != bf) changes = true;
+            bf = newBF;
+        }
+    }
+    return bf;
+}
+
+function runBF(env, bf) {
+    return executeParsedBF(env, parseBF(bf));
+}
+
+function parseConstants(txt) {
+    let result = new Map();
+    for (let line of txt.split("\n")) {
+        line = line.trim();
+        if (line.length == 0 || line[0] == "#") continue;
+        let groups = /^(\w+)=(\w*)$/.exec(line);
+        if (groups == null) {
+            throw new BFloatParseError("Illegal syntax in constants");
+        }
+        result.set(groups[1], groups[2]);
+    }
+    return result;
+}
+
+function compileMacro(env, macroName, arguments) {
+    let macro = env.macros.find(macro => macro.name == macroName);
+
+    if (!macro) {
+        throw new BFloatParseError(`Macro ${macroName} expected but not found`);
+    }
+
+    function sendError(msg) {
+        throw new BFloatParseError(`In macro ${macroName}: ${msg}`);
+    }
+
+    let txt = macro.content;
+
+    let varsArr = arguments.map((arg, idx) => [idx.toString(), arg]);
+    if (env.constants) {
+        varsArr.push(...env.constants);
+    }
+    let vars = new Map(varsArr);
+
+    txt = txt.replace(/\/\w+\//g, match => {
+        let name = match.slice(1, -1);
+        let val = vars.get(name);
+        if (!val) {
+            sendError(`No variable named ${name} found`);
+        }
+        return val;
+    });
+
+    let tokenRules = [
+        {re: /^([\[\]\+\-,.<>~])/, name: "bf"},
+        {re: /^\ ?\{(\d+)\}/, name: "repeat"},
+        {re: /^(\w+)(?: ?\(([^\ ),]+(?:, *[^\ ),]+)*)\))?/, name: "macro"},
+        {re: /^\s+/, name: "noop"}, // whitespace
+        {re: /^#[^#]*#/, name: "noop"} // comment
+    ];
+
+    let tokens = [];
+
+    let i = 0;
+    while (i < txt.length) {
+        let rest = txt.slice(i);
+        let match = null;
+        let rule = null;
+
+        for (let tokenRule of tokenRules) {
+            match = tokenRule.re.exec(rest);
+            if (match) {
+                rule = tokenRule;
+                break;
+            }
+        }
+
+        if (match == null) {
+            let exerpt = txt.slice(i, i + 10);
+            sendError("Invalid syntax here ->" + exerpt);
+        }
+
+        i += match[0].length;
+        if (rule.name == "noop") continue;
+        tokens.push({name: rule.name, parts: match.slice(1)});
+    }
+
+    let parseRules = [
+        {name: "repeat", func: (tokens, i) => {
+            if (i < 1) {
+                sendError("Can't repeat nothing");
+            }
+            let repeatee = tokens[i-1];
+            let token = tokens[i];
+            let count = parseInt(token.parts[0]);
+            return {from: i - 1, to: i, replacement: Array(count).fill(repeatee)}
+        }},
+        {name: "macro", func: (tokens, i) => {
+            let token = tokens[i];
+            let macroName = token.parts[0];
+            let arguments = [];
+            let argStr = token.parts[1];
+            if (argStr != null) {
+                arguments = argStr.split(",").map(arg => arg.trim());
+            }
+            return {from: i, to: i, replacement: [{
+                name: "bf", parts: [compileMacro(env, macroName, arguments)]
+            }]};
+        }}
+    ];
+
+    outerLoop: while (true) {
+        for (let parseRule of parseRules) {
+            for (let i = 0; i < tokens.length; i++) {
+                let token = tokens[i];
+                if (token.name != parseRule.name) continue;
+                let {from, to, replacement} = parseRule.func(tokens, i);
+                tokens.splice(from, to - from + 1, ...replacement);
+                continue outerLoop;
+            }
+        }
+        break;
+    }
+
+    let result = "";
+    for (let token of tokens) {
+        if (token.name != "bf") {
+            sendError("Unmatched "+token.name);
+        }
+        result += token.parts[0];
+    }
+    return result;
+}
+
+function getEnvBF(env) {
+    let compiled = compileMacro(env, "program", []);
+    if (env.onCompiled) env.onCompiled(compiled);
+    let optimized = optimizeTextBF(compiled);
+    if (env.onOptimized) env.onOptimized(optimized);
+    return optimized;
+}
+
+function runEnv(env) {
+    return runBF(env, getEnvBF(env));
+}
--- script.js
+++ script.js
@@ -1,49 +1,27 @@
-async function idle() {
-    await new Promise(resolve => setTimeout(resolve, 0));
-}
-
-function parseBF(bf) {
-    let result = [];
-    let stack = [result];
-    for (let char of bf) {
-        let current = stack.at(-1);
-        if (char == "+") {
-            current.push({action: "incr"});
-        } else if (char == "-") {
-            current.push({action: "decr"});
-        } else if (char == "<") {
-            current.push({action: "left"});
-        } else if (char == ">") {
-            current.push({action: "right"});
-        } else if (char == ".") {
-            current.push({action: "output"});
-        } else if (char == ",") {
-            current.push({action: "input"});
-        } else if (char == "~") {
-            current.push({action: "dump"});
-        } else if (char == "[") {
-            let block = [];
-            current.push({action: "loop", block});
-            stack.push(block);
-        } else if (char == "]") {
-            stack.pop();
-        }
-    }
-    return result;
-}
-
 let lastInput = "";
 let onNextInput = null;
 let cancelExecution = null;
 let isRunning = false;
 let isStopRequested = false;
 
+function sendOutputStr(str) {
+    const consoleTextElem = document.getElementById("console-text");
+    let consoleText = consoleTextElem.value;
+    consoleText += str;
+    consoleTextElem.value = consoleText;
+    lastInput = consoleText;
+}
+
+function showError(error) {
+    sendOutputStr(error+"\n");
+}
+
 function setIsRunning(running) {
     document.getElementById("stop-run").innerText = running ? "Stop" : "Run";
     isRunning = running;
 }
 
-function doDump(mem, i) {
+function outputDump(mem, i) {
     let str = "";
     for (let j in mem) {
         if (j % 8 == 0) str += "\n";
@@ -59,94 +37,7 @@ function doDump(mem, i) {
         if (j == i+7) str += " XD";
         str += "\n";
     }
-    return str;
-}
-
-function wrapNum(n) {
-    n %= 256;
-    if (n < 0) n += 256;
-    return n;
-}
-
-async function runBF(bf, getInput, sendOutput) {
-    let lastYield = Date.now();
-
-    let parsed = parseBF(bf);
-
-    let mem = [0];
-    let i = 0;
-
-    let stack = [parsed];
-    let idxs = [0];
-    while (stack.length > 0) {
-        let current = stack.at(-1);
-        let idx = idxs.at(-1);
-
-        if (current.length == idx) {
-            let now = Date.now();
-            if (now - lastYield > 50) {
-                if (isStopRequested) return;
-                await idle();
-                lastYield = now;
-            }
-            if (mem[i] == 0 || stack.length == 1) {
-                stack.pop();
-                idxs.pop();
-            } else {
-                idxs[idxs.length-1] = 0;
-            }
-            continue;
-        }
-
-        let cmd = current[idx];
-        let action = cmd.action;
-
-        idxs[idxs.length-1] = idx+1;
-
-        if (action == "incr") {
-            mem[i] = wrapNum(mem[i]+1);
-        } else if (action == "decr") {
-            mem[i] = wrapNum(mem[i]-1);
-        } else if (action == "left") {
-            if (i > 0) i--;
-        } else if (action == "right") {
-            i++;
-            while (mem.length <= i) {
-                mem.push(0);
-            }
-        } else if (action == "output") {
-            sendOutput(String.fromCharCode(mem[i]));
-        } else if (action == "input") {
-            let input = await getInput();
-            if (input == -1) return;
-            mem[i] = input;
-            lastYield = Date.now();
-        } else if (action == "dump") {
-            sendOutput(doDump(mem, i));
-        } else if (action == "loop") {
-            if (mem[i] != 0) {
-                stack.push(cmd.block);
-                idxs.push(0);
-            }
-        }
-    }
-}
-
-function optimizeTextBF(bf) {
-    let emptyEquivalents = [
-        "<>", "><", "-+", "+-", 
-        /(?<=\])\[[^\[\]]+\]/g, "[]"
-    ];
-    let changes = true;
-    while (changes) {
-        changes = false;
-        for (let phrase of emptyEquivalents) {
-            let newBF = bf.replaceAll(phrase, "");
-            if (newBF != bf) changes = true;
-            bf = newBF;
-        }
-    }
-    return bf;
+    sendOutputStr(str);
 }
 
 function getMacros() {
@@ -160,117 +51,102 @@ function getMacros() {
     return result;
 }
 
-function* getConstants() {
-    let constantsTxt = document.getElementById("constants-input").value;
-    for (let line of constantsTxt.split("\n")) {
-        if (line.startsWith("#") || line == "") continue;
-        let groups = /^(\w+)=(\w*)$/.exec(line);
-        if (groups == null) {
-            sendError("Illegal syntax in constants");
-        }
-        yield [groups[1], groups[2]];
+function getConstants() {
+    return parseConstants(
+        document.getElementById("constants-input").value
+    );
+}
+
+function checkInputChar() {
+    const consoleTextElem = document.getElementById("console-text");
+    let consoleText = consoleTextElem.value;
+    if (consoleText.startsWith(lastInput) && consoleText.length > lastInput.length) {
+        let char = consoleText[lastInput.length];
+        lastInput = consoleText.slice(0, lastInput.length+1);
+        return char.charCodeAt(0);
+    } else {
+        lastInput = consoleText;
+        return null;
     }
 }
 
-function compileMacro(macros, macroName, arguments) {
-    let macro = macros.find(macro => macro.name == macroName);
+function clearConsole() {
+    lastInput = "";
+    document.getElementById("console-text").value = "";
+}
 
-    if (!macro) {
-        sendError(`Macro ${macroName} expected but not found`);
+async function getInputChar() {
+    while (true) {
+        let char = checkInputChar();
+        if (char != null) return char;
+        let {promise, resolve, reject} = Promise.withResolvers();
+        onNextInput = resolve;
+        cancelExecution = reject;
+        try {
+            await promise;
+        } catch {
+            return -1;
+        } finally {
+            cancelExecution = null;
+            onNextInput = null;
+        }
+    }
+}
+
+function getEnv() {
+    return {
+        macros: getMacros(),
+        constants: getConstants(),
+        onStart: () => {
+            setIsRunning(true);
+            isStopRequested = false;
+            clearConsole();
+            sendOutputStr("Execution started\n");
+        },
+        isStopRequested: () => isStopRequested,
+        sendOutput: sendOutputStr,
+        getInput: getInputChar,
+        outputDump
+    };
+}
+
+async function runCode() {
+    saveToStorage();
+
+    try {
+        await runEnv(getEnv());
+        sendOutputStr("\nExecution finished\n");
+    } catch (e) {
+        showError(e);
     }
 
-    let txt = macro.content;
+    setIsRunning(false);
+}
 
-    let vars = new Map([
-        ...arguments.map((arg, idx) => [idx.toString(), arg]),
-        ...getConstants()
-    ]);
+function showCompiled() {
+    saveToStorage();
+    if (isRunning) return;
 
-    txt = txt.replace(/\/\w+\//g, (match) => {
-        let name = match.slice(1, -1);
-        let val = vars.get(name);
-        if (val) {
-            return val;
-        } else {
-            sendError(`no variable named ${name} found`);
-        }
-    });
-
-    let tokenRules = [
-        {re: /^([\[\]\+\-,.<>~])/, name: "bf"},
-        {re: /^\ ?{(\d+)\}/, name: "repeat"},
-        {re: /^(\w+)(?: ?\(([^\ ),]+(?:, *[^\ ),]+)*)\))?/, name: "macro"},
-        {re: /^\s+/, name: "noop"}, // whitespace
-        {re: /^#[^#]+#/, name: "noop"} // comment
-    ];
-
-    let tokens = [];
-
-    let i = 0;
-    while (i < txt.length) {
-        let rest = txt.slice(i);
-        let match = null;
-        let rule = null;
-        for (let tokenRule of tokenRules) {
-            match = tokenRule.re.exec(rest);
-            if (match) {
-                rule = tokenRule;
-                break;
-            }
-        }
-        if (match == null) {
-            sendError("invalid syntax here ->" + txt.slice(i, i + 10));
-        }
-        i += match[0].length;
-        tokens.push({name: rule.name, parts: match.slice(1)});
+    let bf;
+    try {
+        bf = getEnvBF(getEnv());
+    } catch (e) {
+        showError(e);
+        return;
     }
+    
+    clearConsole();
+    sendOutputStr("Compiled output\n");
+    sendOutputStr(bf);
+}
 
-    let parseRules = [
-        {name: "noop", func: (tokens, i) => {
-            return {from: i, to: i, replacement: []};
-        }},
-        {name: "repeat", func: (tokens, i) => {
-            if (i < 1) sendError("cannot repeat nothing");
-            let repeatee = tokens[i-1];
-            let token = tokens[i];
-            let count = parseInt(token.parts[0]);
-            return {from: i - 1, to: i, replacement: Array(count).fill(repeatee)}
-        }},
-        {name: "macro", func: (tokens, i) => {
-            let token = tokens[i];
-            let macroName = token.parts[0];
-            let arguments = [];
-            let argStr = token.parts[1];
-            if (argStr != null) {
-                arguments = argStr.split(",").map(arg => arg.trim());
-            }
-            return {from: i, to: i, replacement: [{
-                name: "bf", parts: [compileMacro(macros, macroName, arguments)]
-            }]};
-        }}
-    ];
-
-    outerLoop: while (true) {
-        for (let parseRule of parseRules) {
-            for (let i in tokens) {
-                let token = tokens[i];
-                if (token.name != parseRule.name) continue;
-                let {from, to, replacement} = parseRule.func(tokens, i);
-                tokens.splice(from, to - from + 1, ...replacement);
-                continue outerLoop;
-            }
-        }
-        break;
+function stopCode() {
+    if (!isRunning) return;
+    if (cancelExecution != null) {
+        cancelExecution();
+    } else {
+        isStopRequested = true;
     }
-
-    let result = "";
-    for (let token of tokens) {
-        if (token.name != "bf") {
-            sendError(`Invalid token name in final result: '${token.name}'`);
-        }
-        result += token.parts[0];
-    }
-    return result;
 }
 
 function addMacro(name=null, content=null) {
@@ -293,94 +169,6 @@ function saveToStorage() {
         macros: getMacros(),
         constants: document.getElementById("constants-input").value
     }));
-}
-
-function getInputChar() {
-    const consoleTextElem = document.getElementById("console-text");
-    let consoleText = consoleTextElem.value;
-    if (consoleText.startsWith(lastInput) && consoleText.length > lastInput.length) {
-        let char = consoleText[lastInput.length];
-        lastInput = consoleText.slice(0, lastInput.length+1);
-        return char.charCodeAt(0);
-    } else {
-        lastInput = consoleText;
-        return null;
-    }
-}
-
-function sendOutputStr(str) {
-    const consoleTextElem = document.getElementById("console-text");
-    let consoleText = consoleTextElem.value;
-    consoleText += str;
-    consoleTextElem.value = consoleText;
-    lastInput = consoleText;
-}
-
-function sendError(error) {
-    sendOutputStr(`\nError: ${error}\n\n`);
-    throw new Error(error);
-}
-
-function clearConsole() {
-    lastInput = "";
-    document.getElementById("console-text").value = "";
-}
-
-function transpileToBF() {
-    try {
-        return optimizeTextBF(compileMacro(getMacros(), "program", []));
-    } catch (e) {
-        return null;
-    }
-}
-
-async function runCode() {
-    saveToStorage();
-    if (isRunning) return;
-    let bf = transpileToBF();
-    if (bf == null) return;
-    setIsRunning(true);
-    isStopRequested = false;
-    clearConsole();
-    sendOutputStr("Execution started\n");
-    await runBF(bf, async () => {
-        while (true) {
-            let char = getInputChar();
-            if (char != null) return char;
-            let {promise, resolve, reject} = Promise.withResolvers();
-            onNextInput = resolve;
-            cancelExecution = reject;
-            try {
-                await promise;
-            } catch {
-                return -1;
-            } finally {
-                cancelExecution = null;
-                onNextInput = null;
-            }
-        }
-    }, sendOutputStr);
-    setIsRunning(false);
-    sendOutputStr("\nExecution finished\n");
-}
-
-function stopCode() {
-    if (!isRunning) return;
-    if (cancelExecution != null) {
-        cancelExecution();
-    } else {
-        isStopRequested = true;
-    }
-}
-
-function showCompiled() {
-    saveToStorage();
-    if (isRunning) return;
-    let bf = transpileToBF();
-    if (bf == null) return;
-    clearConsole();
-    sendOutputStr("Compiled output\n");
-    sendOutputStr(bf);
 }
 
 function exportSave() {
